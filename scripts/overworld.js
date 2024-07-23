1b0c0ab9f638bebde19e852e656fa11660ebf635
Max Leeming <max.leeming@gmail.com>
2024-07-23 17:08:52 -0400

    v5.0 - Character Animations

--- scripts/overworld.js
+++ scripts/overworld.js
@@ -33,7 +33,7 @@ class Overworld {
     }
 
     init() {
-        this.map = new OverworldMap(window.OverworldMaps.Kitchen)
+        this.map = new OverworldMap(window.OverworldMaps.DemoRoom)
 
         this.directionInput = new DirectionInput()
         this.directionInput.init()
